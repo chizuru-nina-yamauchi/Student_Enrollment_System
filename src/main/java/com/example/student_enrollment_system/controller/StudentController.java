7552ae5c7f9d460139adc82c20f474c5a25d957f
Chizuru <chizuru.yamauchi@dci-student.org>
2024-05-07 14:37:39 +0200

    add html files and connect

--- src/main/java/com/example/student_enrollment_system/controller/StudentController.java
+++ src/main/java/com/example/student_enrollment_system/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.example.student_enrollment_system.controller;
 
 
+import com.example.student_enrollment_system.model.Student;
 import com.example.student_enrollment_system.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -11,6 +16,24 @@ public class StudentController {
     @Autowired
     private StudentService service;
 
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        model.addAttribute("students", service.getAllStudents());
+        return "home";
+    }
+
+    @GetMapping("/enroll")
+    public String showEnrollForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "enroll";
+    }
+
+    @PostMapping("/enroll")
+    public String submitStudentForm(@ModelAttribute("student") Student student) {
+        service.saveStudent(student);
+        return "redirect:/";
+    }
 
 
 }
--- src/main/java/com/example/student_enrollment_system/model/Student.java
+++ src/main/java/com/example/student_enrollment_system/model/Student.java
@@ -1,26 +1,63 @@
 package com.example.student_enrollment_system.model;
 
 import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
+
 
 @Entity
 @Table(name = "students")
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date enrollmentDate;
+
+    private String courses;
+
+    public Student() {
+    }
+
+    public Student(String name, Date enrollmentDate, String courses) {
+        this.name = name;
+        this.enrollmentDate = enrollmentDate;
+        this.courses = courses;
+    }
 
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
     }
 
 }
